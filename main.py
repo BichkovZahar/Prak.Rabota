242833b9de3c6a352a26169bb846d7e69ed7af8d
dev <Bichk_di78@student.itstep.org>
2023-05-02 20:44:15 +0300

    EX_3

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import time
-def time_fuction(func):
-    def wrapper(*args , **kwargs):
-        start_time = time.time()
-        rezult = func(*args  , **kwargs)
-        end_time = time.time()
-        print("Час виконання функції:" , round(end_time - start_time , 2))
-        return rezult
+def finish(func):
+    cache = {}
+    def wrapper(*args , **kwars):
+        if args in cache :
+            return cache[args]
+        else:
+            rezult = func(*args)
+            cache[args] = rezult
+            return rezult , cache
     return wrapper
-
-@time_fuction
-def example_function(x):
-    time.sleep(1)
-    return x
-print(example_function("Hello Pypsik"))
+@finish
+def example_function(x , y):
+    return x * y
+print(example_function(5 , 3))
